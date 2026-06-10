78cfdd7083a4f99a2a2fe803255cec3b05688bb3
Ilham Mohammed <ilham21495@gmail.com>
2026-06-10 10:37:53 +0530

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,28 @@
+
+package TestTracker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/tasktracker";
+    private static final String USER = "root";
+    private static final String PASSWORD = "2004";
+
+    public static Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(
+                    URL, USER, PASSWORD);
+
+        } catch (Exception e) {
+            System.out.println("Connection Error : "
+                    + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package TestTracker;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new TaskFrame().setVisible(true);
+        });
+    }
+}
